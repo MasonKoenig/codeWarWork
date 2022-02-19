02a5d3b75743df91008ce0b0664a8f34a2216909
Mason Koenig <koenig.mason@gmail.com>
2022-02-19 14:52:21 -0500

    Summing series and formatting numbers

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-function DNAStrand(dna) {
-  const array = dna.split('');
-  const newArray = array.map((x) => {
-    if (x === 'A') return 'T';
-    if (x === 'T') return 'A';
-    if (x === 'G') return 'C';
-    if (x === 'C') return 'G';
-  });
-  return newArray.join('');
+function SeriesSum(n) {
+  let denominator = 1;
+  let result = 0;
+  if (n === 0) return '0.00';
+  for (let i = 0; i < n; i++) {
+    result += 1 / denominator;
+    denominator += 3;
+  }
+  result = (Math.round(result * 100) / 100).toFixed(2);
+  return result.toString();
 }
 
-console.log(DNAStrand('ATTGC'));
+console.log(SeriesSum(1));
+console.log('0.00');
