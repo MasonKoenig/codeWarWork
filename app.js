8d04fde127491ebb4d6b05ddb4884256a54d99ad
Mason Koenig <koenig.mason@gmail.com>
2022-02-19 14:57:55 -0500

    Simple calculator function

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-function SeriesSum(n) {
-  let denominator = 1;
-  let result = 0;
-  if (n === 0) return '0.00';
-  for (let i = 0; i < n; i++) {
-    result += 1 / denominator;
-    denominator += 3;
-  }
-  result = (Math.round(result * 100) / 100).toFixed(2);
-  return result.toString();
+function basicOp(operation, value1, value2) {
+  if (operation === '+') return value1 + value2;
+  if (operation === '-') return value1 - value2;
+  if (operation === '*') return value1 * value2;
+  if (operation === '/') return value1 / value2;
 }
 
-console.log(SeriesSum(1));
-console.log('0.00');
+console.log(basicOp('+', 1, 3));
